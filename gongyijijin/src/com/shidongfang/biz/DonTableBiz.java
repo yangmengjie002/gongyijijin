0590e1ffdcbe9007e4303b71d0ab8393fa8968ab
skyheaber1 <2489672834@qq.com>
2017-06-20 20:21:39 +0800

    6.20

--- gongyijijin/src/com/shidongfang/biz/DonTableBiz.java
+++ gongyijijin/src/com/shidongfang/biz/DonTableBiz.java
@@ -1,21 +1,21 @@
-package com.shidongfang.dao;
+package com.shidongfang.biz;
 
 import java.util.List;
 import java.util.Map;
 
 import com.entity.DonTable;
 /**
- * @描述 dao接口
+ * @描述 用户业务接口
  * @作者
  * @公司
  * @时间 2017-6-20
  */
-public interface DonTableDao {
+public interface DonTableBiz {
 	/**
-	 * 接口方法 
 	 * 增删改查
+	 * 
 	 * */
-	public boolean addDonTable(DonTable dt);//增加
+	public void addDonTable(DonTable dt);//增加
 	public void deleteDonTable(DonTable dt);//删除
 	public void updateDonTable(DonTable dt);//修改
 	public List<Map<String, Object>> select();//查询
--- gongyijijin/src/com/shidongfang/util/BaseAction.java
+++ gongyijijin/src/com/shidongfang/util/BaseAction.java
@@ -1,11 +1,19 @@
-package shidongfang.util;
+package com.shidongfang.util;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.struts2.ServletActionContext;
 
+
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -14,53 +22,76 @@ import com.opensymphony.xwork2.ActionSupport;
  *
  */
 public class BaseAction extends ActionSupport{
-	/**
-	 * 向request中放入键值对str=obj。
-	 * @param str
-	 * @param obj
-	 */
-	public static void setRequestAttribute(String str,Object obj){
-		ServletActionContext.getRequest().setAttribute(str, obj);
-	}
 	
-	/**
-	 * 从request中获取str的值。
-	 * @param str
-	 * @return
-	 */
-	public static Object getRequestAttribute(String str){
-		Object attribute = ServletActionContext.getRequest().getAttribute(str);
-		return attribute;
+	private Map request = (Map) ActionContext.getContext().get("request");
+	private Map session = (Map) ActionContext.getContext().getSession();
+	String uploadDir="upload";
+	public Map getRequest() {
+		return request;
 	}
-	/**
-	 * 把键值对str：obj放入session中。
-	 * @param str
-	 * @param obj
-	 */
-	public static void setSessionAttribute(String str,Object obj){
-		ServletActionContext.getRequest().getSession().setAttribute(str, obj);
+	public void setRequest(Map request) {
+		this.request = request;
 	}
-	
-	/**
-	 * 从session中获取str的值。
-	 * @param str
-	 * @return
-	 */
-	public static Object getSessionAttribute(String str){
-		return ServletActionContext.getRequest().getSession().getAttribute(str);
+	public Map getSession() {
+		return session;
 	}
-	
-	/**
-	 * 把str响应到前端。
-	 * @param str
-	 */
-	public void getWriter(String str){
-		try {
-			ServletActionContext.getResponse().getWriter().write(str);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void setSession(Map session) {
+		this.session = session;
+	}
+	/*
+	 * 删除服务器多余的图片
+	 * 
+	 * 
+	 * **/
+	public  void deletePicture(List<String> pathList) {
+		String path=ServletActionContext.getServletContext().getRealPath(uploadDir);
+		File file = new File(path);
+		File[] listFiles = file.listFiles();
+		for (File uploadfile : listFiles) {
+			String name = uploadfile.getName();
+			if(!pathList.contains(name)){
+				uploadfile.delete();
+			}
 		}
+		
+
 	}
+	public  String uploadFile(String uploadFileName,File upload ) {
+		//获取上传文件在服务器上的绝对路径
+				
+				String[] names=uploadFileName.split("\\.");//将文件以.分割成一个数组xxxx.txt
+				String tempNum=(int)(Math.random()*100000)+"";
+				uploadFileName=tempNum +System.currentTimeMillis()+"."+names[names.length-1];
+				String uploadFile=ServletActionContext.getServletContext().getRealPath(uploadDir)+File.separator+uploadFileName;
+				try {
+					FileInputStream fis = new FileInputStream(upload);
+					FileOutputStream fos = new FileOutputStream(uploadFile);
+					byte[] buf= new byte[1024];
+					int len=0;
+						while ((len=fis.read(buf))!=-1) {
+							fos.write(buf,0,len);
+						}
+						fis.close();
+						fos.flush();
+						fos.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return uploadDir+File.separator+uploadFileName;
+
+	}
+	
+	
+	public static void main(String[] args) {
+		String str="xxxx.txt";
+		String tempNum=(int)(Math.random()*100000)+"";
+		String[] split = str.split("\\.");
+		System.out.println(split[0]+"|||||||"+split[1]);
+		System.out.println(tempNum);
+	}
+	
+	
 	
 }
